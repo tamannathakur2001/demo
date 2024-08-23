bae003f97f9d407747d20889b2ad6e3009f3a61e
tamannathakur2001 <contact.ttamanna@gmail.com>
2024-08-23 18:32:14 +0530

    fixed issue in AWS ec2

--- application2/app.py
+++ application2/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify
 import requests
+import os
 
 app = Flask(__name__)
+
+minikube_ip = os.getenv('MINIKUBE_IP')
+app1_nodeport = os.getenv('APP1_NODEPORT')
+
+app1_url = f"http://{minikube_ip}:{app1_nodeport}/application1/msg-response"
 
 def reverse_string(string):
     rev = ""
@@ -11,10 +17,17 @@ def reverse_string(string):
 
 @app.route('/application2/reverse-msg-response', methods=['GET'])
 def reverse_message():
-    response = requests.get('http://application1:5000/application1/msg-response')
-    data = response.json()
-    data["message"] = reverse_string(data["message"])
-    return jsonify(data)
+    try:
+        response = requests.get(app1_url)
+
+        if response.status_code == 200:
+            data = response.json()
+            data["message"] = reverse_string(data["message"])
+            return jsonify(data)
+        else:
+            return f"App1 returned an error: HTTP {response.status_code}", response.status_code
+    except requests.RequestException as e:
+        return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- script.sh
+++ script.sh
@@ -3,19 +3,20 @@
 echo "###################### START DEPLOYMENT ########################"
 
 #################################
-#  SCRIPT INPUT ARGS			#
+#  SCRIPT INPUT ARGS                    #
 #################################
 
 DOCKER_USERNAME=$1 # Please provide your Docker Username as an script argument
 DOCKER_PASSWORD=$2 # Please provide your Docker Password as an script argument
+NAMESPACE=$3 # Provide NAMESPACE as per your choice that you wanted to deploy this aplication to.
 
 #################################
-#  INITIALIZATION STAGE			#
+#  INITIALIZATION STAGE                 #
 #################################
 echo "INITIALIZING VARIABLES"
+MINIKUBE_IP=$(minikube ip)
 HELM_RELEASE_NAME="my-apps"
 HELM_CHART_PATH="./webapp"
-NAMESPACE="village"
 APPLICATION1_IMAGE="application1"
 APPLICATION2_IMAGE="application2"
 APP1_DEPLOYMENT="app1-deployment"
@@ -36,9 +37,9 @@ fi
 ## Using SED command to dynamically chaning the Image Name in docker compose file
 sed -i "s|image: [^/]*/$APPLICATION1_IMAGE|image: \"$APP1_IMG_NAME|g" $HELM_CHART_PATH/values.yaml
 sed -i "s|image: [^/]*/$APPLICATION2_IMAGE|image: \"$APP2_IMG_NAME|g" $HELM_CHART_PATH/values.yaml
-
+sed -i "s|MINIKUBE_IP: .*|MINIKUBE_IP: \"$MINIKUBE_IP\"|g" $HELM_CHART_PATH/templates/app-conf.yml
 #################################
-#        BUILD STAGE			#
+#        BUILD STAGE                    #
 #################################
 echo "Building Docker images..."
 docker build --no-cache -t $APP1_IMG_NAME:latest ./application1
@@ -51,48 +52,37 @@ docker push $APP1_IMG_NAME:latest
 docker push $APP2_IMG_NAME:latest
 
 #################################
-#    HELM DEPLOY STAGE			#
+#    HELM DEPLOY STAGE                  #
 #################################
 # Deploy to Kubernetes using Helm
 echo "Deploying to Kubernetes..."
 helm upgrade --install $HELM_RELEASE_NAME $HELM_CHART_PATH --namespace $NAMESPACE
 
 echo "Waiting for deployment $APP1_DEPLOYMENT to be available..."
-kubectl wait --for=condition=available --timeout=120s deployment/$APP1_DEPLOYMENT
+kubectl wait --for=condition=available --timeout=120s deployment/$APP1_DEPLOYMENT -n $NAMESPACE
 
 echo "Waiting for deployment $APP2_DEPLOYMENT to be available..."
-kubectl wait --for=condition=available --timeout=120s deployment/$APP2_DEPLOYMENT
+kubectl wait --for=condition=available --timeout=120s deployment/$APP2_DEPLOYMENT -n $NAMESPACE
 
-## Wait for services to be ready
-# echo "Waiting for services to be ready..."
-# sleep 30
+APP1_NODEPORT=$(kubectl get service app1-service -o jsonpath='{.spec.ports[0].nodePort}' -n $NAMESPACE)
+APP2_NODEPORT=$(kubectl get service app2-service -o jsonpath='{.spec.ports[0].nodePort}' -n $NAMESPACE)
 
-kubectl port-forward svc/app1-service 5000:5000 &
-APP1_PID=$!
-kubectl port-forward svc/app2-service 5001:5001 &
-APP2_PID=$!
-
-sleep 5
-echo ""
 
 #################################
-#      HTTP RESPONSE STAGE		#
+#      HTTP RESPONSE STAGE              #
 #################################
 echo "========Application1 Response ========="
 echo "Fetching HTTP response from application1..."
-curl -s http://localhost:5000/api/message
+curl -s http://${MINIKUBE_IP}:${APP1_NODEPORT}/application1/msg-response
 
 echo ""
 
 echo "Fetching HTTP response from application2..."
-curl -s http://localhost:5001/api/reverse-message
+curl -s http://${MINIKUBE_IP}:${APP2_NODEPORT}/application2/reverse-msg-response
 echo ""
 
-kill $APP1_PID
-kill $APP2_PID
-
 #################################
-#   CLEAN UP ALL UNUSED IMAGES	#
+#   CLEAN UP ALL UNUSED IMAGES  #
 #################################
 # Uncomment code if you wnat to cleanup older unused images from minikube. I used it as while testing there were many unused images got created and that not great.
 
